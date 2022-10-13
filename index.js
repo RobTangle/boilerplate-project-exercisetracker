09fcc1593b966ed76b99fcc2fe1440229209f02f
Robert0101 <rob.tangled@gmail.com>
2022-10-13 20:55:53 -0300

    docs/Instalar paquetes y armar pseudo código para empezar

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+// Conectar DB en MongoDB
+// Armar schemas de:
+
+// Excercise:
+// {
+//   username: "fcc_test",
+//   description: "test",
+//   duration: 60,
+//   date: "Mon Jan 01 1990",
+//   _id: "5fb5853f734231456ccb3b05"
+// }
+
+// User:
+// {
+//   username: "fcc_test",
+//   _id: "5fb5853f734231456ccb3b05"
+// }
+
+// Log:
+// {
+//   username: "fcc_test",
+//   count: 1,
+//   _id: "5fb5853f734231456ccb3b05",
+//   log: [{
+//     description: "test",
+//     duration: 60,
+//     date: "Mon Jan 01 1990",
+//   }]
+// }
